45d0996cf54f2fea73218e9230e401c5108acb7e
Szu-Kai Hsu (@brucehsu) <brucehsu@gmail.com>
2020-01-05 19:09:05 +0100

    style: use prettier with eslint

--- tests/lib/xml.spec.js
+++ tests/lib/xml.spec.js
@@ -1,24 +1,26 @@
-jest.mock('node-fetch');
+jest.mock("node-fetch");
 
-const fs = require('fs');
-const path = require('path');
-const fetch = require('node-fetch');
-const { fetchFeed } = require('../../lib/xml');
+const fs = require("fs");
+const path = require("path");
+const fetch = require("node-fetch");
+const { fetchFeed } = require("../../lib/xml");
 
-describe('XML Parsing', () => {
-    const mockAnchorId = 'MOCK_ID';
-    const mockFeedPath = path.join(__dirname, '..', 'mocks', 'feed.xml');
-    test('fetch podcast feed and parse it into object', async() => {
-        fetch.mockResolvedValue({
-            text: jest.fn().mockResolvedValue(fs.readFileSync(mockFeedPath, 'utf8'))
-        });
-        const result = await fetchFeed(mockAnchorId);
-        const { channel } = result.rss;
-        expect(fetch.mock.calls.length).toBe(1);
-        expect(fetch.mock.calls[0][0]).toBe(`https://anchor.fm/s/${mockAnchorId}/podcast/rss`);
-        expect(channel.title).toBe('Taiwanese Expats in Tech');
-        expect(channel['itunes:author']).toBe('Szu-Kai Hsu');
-        expect(channel.lastBuildDate).toBe('Sun, 05 Jan 2020 15:50:16 GMT');
-        expect(channel.item.length).toBe(3);
+describe("XML Parsing", () => {
+  const mockAnchorId = "MOCK_ID";
+  const mockFeedPath = path.join(__dirname, "..", "mocks", "feed.xml");
+  test("fetch podcast feed and parse it into object", async () => {
+    fetch.mockResolvedValue({
+      text: jest.fn().mockResolvedValue(fs.readFileSync(mockFeedPath, "utf8"))
     });
+    const result = await fetchFeed(mockAnchorId);
+    const { channel } = result.rss;
+    expect(fetch.mock.calls.length).toBe(1);
+    expect(fetch.mock.calls[0][0]).toBe(
+      `https://anchor.fm/s/${mockAnchorId}/podcast/rss`
+    );
+    expect(channel.title).toBe("Taiwanese Expats in Tech");
+    expect(channel["itunes:author"]).toBe("Szu-Kai Hsu");
+    expect(channel.lastBuildDate).toBe("Sun, 05 Jan 2020 15:50:16 GMT");
+    expect(channel.item.length).toBe(3);
+  });
 });
